20b0a808bb8e9c6d695a972e0f6307708946ac18
phodzo nagana <phodzonagz12@gmail.com>
2024-12-04 15:00:30 +0200

    added routing on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.scss";
+import "swiper/swiper-bundle.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
+  );
 }
 
 export default App;
