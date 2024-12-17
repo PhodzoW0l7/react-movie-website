e8b25faaee3670e75f9800d2587f1c5c0ac47531
Phodzo Nagana <130413165+PhodzoW0l7@users.noreply.github.com>
2024-12-17 14:27:34 +0200

    adjusted the navigation to be horizontally aligned

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import '../header/header.scss';
 import { Link, useLocation } from 'react-router-dom';
+import logo from '../../assets/WatchFlix-removebg-preview.png';
 
 const headerNav = [
   {
@@ -20,11 +21,25 @@ const headerNav = [
 const Header = () => {
   const { pathname } = useLocation();
   const headerRef = useRef(null);
-  const active = headerNav.findIndex((nav) => nav.path === pathname); // Fixed the incomplete line
+
+  // Find the index of the active nav item
+  const active = headerNav.findIndex((e) => e.path === pathname);
 
   return (
-    <div>
-      Header
+    <div ref={headerRef} className="header">
+      <div className="header__wrap container">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <Link to="/">WatchFlix</Link>
+        </div>
+        <ul className="header__nav">
+          {headerNav.map((e, i) => (
+            <li key={i} className={`${i === active ? 'active' : ''}`}>
+              <Link to={e.path}>{e.display}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
