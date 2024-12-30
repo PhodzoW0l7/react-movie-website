fce0f13817e823c1ca60b26ce62c709bb4ef0282
Phodzo Nagana <130413165+PhodzoW0l7@users.noreply.github.com>
2024-12-31 00:21:42 +0200

    created a modal componenent

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+import './modal.scss'
+
+const modal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Modal.propTypes={
+    active:PropTypes.bool,
+    id:PropTypes.string
+}
+
+export default modal
