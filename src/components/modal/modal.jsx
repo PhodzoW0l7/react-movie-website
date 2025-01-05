cb418507b035c7dcade43ca3fcf25085036383fc
Phodzo Nagana <130413165+PhodzoW0l7@users.noreply.github.com>
2025-01-05 22:33:30 +0200

    finished modal component

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import propTypes from 'prop-types'
 
 import './modal.scss'
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 const modal = () => {
+
+  const [active, setActive]= useState(flase);
+
+  useEffect(()=> {
+    setActive(props.active);
+  }, [props.active]);
+
+
   return (
-    <div>
-      
+    <div id={props.id} className={`modal ${active ? 'active' : ''}`}>
+      {props/Children}
     </div>
   )
 }
@@ -14,6 +24,30 @@ const modal = () => {
 Modal.propTypes={
     active:PropTypes.bool,
     id:PropTypes.string
+}
+
+const contentRef = useRef(null);
+
+const closeModal=() => {
+  contentRef.current.parentNode.classlist.remove('active');
+  if(props.onClose) props.onClose();
+}
+
+export const ModalContent = props => {
+  return (
+    <div ref={contentRef} className="modal__content">
+      {props.Children}
+      <div className="modal__content__close" onClick={closeModal}>
+        <i className="bx bx-x">
+        </i>
+
+      </div>
+    </div>
+  )
+}
+
+ModalContent.propTypes = {
+  onClose: PropTypes.func
 }
 
 export default modal
